5b8c770c698136776a5caf5b59914fe49b0feb67
ORLANDO DOS SANTOS DOURADO <62121619+OrlandoSD@users.noreply.github.com>
2020-07-17 19:34:10 -0300

    complemento da aula de hoje

---
+++ man.js
@@ -0,0 +1,34 @@
+
+var count = 0;
+while(count <= 5){
+    console.log(count);
+    alert(count);
+    count++;
+}
+
+//var idade = 18;
+//var idade = prompt("Qual a sua idade?");
+//if(idade>= 18){
+  //  alert("maior de idade");
+//}else{
+  //  alert("menor de idade");
+
+//};
+
+
+//var frutas = [{nome: "maça", cor: "vermelha"}, {nome: "Uva", cor:"Roxa"}]
+//console.log(frutas);
+//alert(frutas[1].nome)
+//var lista = ["maça", "pêra", "laranja"];
+//lista.push("Uva");
+//lista.pop();
+
+//console.log(lista);
+//cosole.log(lista.toString());
+//console.log(lista.join("   "));
+
+//var nome = "Orlando dos Santos Dourado";
+//var idade = 53;
+//var idade2 = 19
+//alert(nome  + " tem " + idade +  " anos ");
+//alert(idade + idade2)
